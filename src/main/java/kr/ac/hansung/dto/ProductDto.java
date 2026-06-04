2f1129a0d540e2e518b813222af0d2290f8e55d7
Hoy02 <khoyoung02@naver.com>
2026-06-04 18:07:28 +0900

    feat: 수정 페이지 작성 및 수정 기능 추가 구현

--- src/main/java/kr/ac/hansung/dto/ProductDto.java
+++ src/main/java/kr/ac/hansung/dto/ProductDto.java
@@ -2,13 +2,21 @@ package kr.ac.hansung.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
 
 @Getter
 @Setter
 public class ProductDto {
 
+    @NotBlank(message = "상품명은 필수입니다.")
     private String name;
+
+    @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
     private int price;
-    private String description;
+
+    @Min(value = 0, message = "재고는 0개 이상이어야 합니다.")
     private int stock;
+
+    private String description;
 }
